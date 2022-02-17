f1697a7f53f3d25cbafe8fd6890fb4322b0f085f
Okalash <kalachnikovsasha@gmail.com>
2022-02-17 23:20:45 +0200

    read from file add, add None type for curator column

--- parse.py
+++ parse.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os.path
 from openpyxl import load_workbook
 
+
 def excel_writer(stuff_dict):
     # dataframe init
     df = pd.DataFrame(stuff_dict, index=[0])
@@ -16,7 +17,7 @@ def excel_writer(stuff_dict):
             df.to_excel(writer, index=False, startrow=writer.sheets['Sheet1'].max_row, header=False)
 
 
-def get_prop(soup, url):
+def get_prop(soup, url, correct_cat):
     """
     get the needs properties of stuff
     """
@@ -26,7 +27,8 @@ def get_prop(soup, url):
     code_stuff_code = soup.find('div', class_='product__rating').find('p', class_='product__code detail-code')
     seller_stuff_code = soup.find('div', class_='product-seller__info').find('strong', class_='ng-star-inserted')
     # not work normal, need the final category
-    category_link = soup.find('ul', class_='breadcrumbs ng-star-inserted').find('li', class_='breadcrumbs__item ng-star-inserted').\
+    category_link = soup.find('ul', class_='breadcrumbs ng-star-inserted').find('li',
+                                                                                class_='breadcrumbs__item ng-star-inserted'). \
         find('a', class_='breadcrumbs__link').get('href')
 
     # result names
@@ -39,20 +41,38 @@ def get_prop(soup, url):
         'Код товара': code_stuff,
         'Название товара': name_stuff,
         'Ссылка на категорию': category_link,
-        'Ссылка на товар' : url,
-        'Продавец': seller_stuff
+        'Ссылка на товар': url,
+        'Продавец': seller_stuff,
+        'Куратор': None,
+        'Корректная категория': correct_cat
     }
     excel_writer(about_stuff_dict)
 
 
 output_file = './res.xlsx'
-while True:
-    url = input('Put the url:\n')
-    if url == 'q':
-        break
-    else:
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'lxml')
-        get_prop(soup, url)
+input_file = './input_links.txt'
+""""
+In file input_links.txt need to push links format:
+    link_stuff, correct_category_ulr
+    
+Each new transfer from new line
+"""
+with open(input_file, 'r') as file:
+    for url_pair in file.readlines():
+        url_line = url_pair.split(',')
+        request_url_line = requests.get(url_line[0])
+        soup = BeautifulSoup(request_url_line.text, 'lxml')
+        get_prop(soup, url_line[0], url_line[1])
+        print('Link is ready')
+
+# while True:
+#     url = input('Put the url:\n')
+#     if url == 'q':
+#         break
+#     else:
+#         correct_cat = input('Correct category:\n')
+#         r = requests.get(url)
+#         soup = BeautifulSoup(r.text, 'lxml')
+#         get_prop(soup, url, correct_cat)
 
 # print(r.text)
